554e508384a424e7cd2f41468f6487f630d6f9db
amunitac <amunitac@uc.cl>
2023-11-21 18:44:20 -0300

    fix: added JWT_SECRET to ci v3

--- api/controllers/serviceController.js
+++
@@ -1,3 +0,0 @@
-exports.home = async (req, res) => {
-  res.send('Hello from the Express API!');
-};
--- api/services/auth.js
+++ api/services/auth.js
@@ -1,6 +1,7 @@
 const jwtgenerator = require('jsonwebtoken');
 
 function generateToken(user) {
+  console.log('JWT SECRET', process.env.JWT_SECRET);
   return new Promise((resolve, reject) => {
     jwtgenerator.sign(
       { sub: user.id, name: user.username, role: user.role },
